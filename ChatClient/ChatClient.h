e3fe7fa8770a147ea4bd71b076cf9f0b80a993e4
Duc Lam <dinhduclam2001@gmail.com>
2022-06-12 22:52:58 +0700

    done

---
+++ ChatClient/ChatClient.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "resource.h"
+
+#define WM_SOCKET_LOGIN WM_USER + 1
+#define WM_SOCKET_CHATCLIENT WM_USER + 2
+
+#define CONNECT "[CONNECT]"
+#define DISCONNECT "[DISCONNECT]"
+#define SEND "[SEND]"
+#define LIST "[LIST]"
+#define ERROR "[ERROR]"
+#define MESSAGE_ALL "[MESSAGE_ALL]"
+#define MESSAGE "[MESSAGE]"
+#define USER_CONNECT "[USER_CONNECT]"
+#define USER_DISCONNECT "[USER_DISCONNECT]"
